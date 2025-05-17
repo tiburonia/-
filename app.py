728e69d76596286258a874bb307f4792c2ba83ed
tiburonia <junhuck7150@gmail.com>
2025-05-17 10:21:54 +0000

    if문 제거

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def reserve():
     print(f"📥 예약 수신: {name}, {phone}, {date}, {time}, {people}", flush=True)
     return f"{name}님의 예약이 완료되었습니다!"
 
-if __name__ == "__main__":
-    print("🚀 서버 실행 중입니다람쥐: http://localhost:5000", flush=True)
-    app.run(debug=True)
+print("🚀 서버 실행 중입니다람쥐: http://localhost:5000", flush=True)
+app.run(debug=True)
 
 
