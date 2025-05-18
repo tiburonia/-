f34ac8b1611b4bbb29d3e7356ce29ccba2c3bd11
tiburonia <junhuck7150@gmail.com>
2025-05-18 07:07:05 +0000

    수정

--- hello.py
+++ hello.py
@@ -6,3 +6,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route("/hello/<name>")
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route("/")
+def index():
+    return render_template("hello.html", name="게스트")
